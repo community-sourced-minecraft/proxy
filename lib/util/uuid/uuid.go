372a3a9d4825d38c6c3b16e5bec4e7a2a6f3c851
Edit <167678993+tojaedit@users.noreply.github.com>
2024-04-25 21:41:28 +0200

    feat: uuid lib update
    UUID to Username

--- lib/util/uuid/uuid.go
+++ lib/util/uuid/uuid.go
@@ -20,6 +20,21 @@ func UsernameToUUID(username string) (string, error) {
 	return *uuid.ID, nil
 }
 
+func UUIDtoUsername(UUID string) (string, error) {
+	res, err := http.Get("https://sessionserver.mojang.com/session/minecraft/profile/" + UUID)
+	if err != nil {
+		return "", err
+	}
+
+	var username MCResponse
+
+	if err := json.NewDecoder(res.Body).Decode(&username); err != nil {
+		return "", err
+	}
+
+	return *username.Name, nil
+}
+
 type MCResponse struct {
 	Name  *string `json:"name"`
 	ID    *string `json:"id"`
