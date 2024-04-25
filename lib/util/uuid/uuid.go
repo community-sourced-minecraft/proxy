38fcd9318b2d3d01d12fe69330d72d387fddf410
DevMiner <devminer@devminer.xyz>
2024-04-26 01:30:53 +0200

    refactor(whitelist): clean up

--- lib/util/uuid/uuid.go
+++ lib/util/uuid/uuid.go
@@ -2,19 +2,31 @@ package uuid
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+type MCResponse struct {
+	Name  *string `json:"name"`
+	ID    *string `json:"id"`
+	Error *string `json:"errorMessage"`
+	Path  *string `json:"path"`
+}
 
 func UsernameToUUID(username string) (string, error) {
 	res, err := http.Get("https://api.mojang.com/users/profiles/minecraft/" + username)
 	if err != nil {
 		return "", err
 	}
+	defer res.Body.Close()
 
 	var uuid MCResponse
-
 	if err := json.NewDecoder(res.Body).Decode(&uuid); err != nil {
 		return "", err
+	}
+
+	if uuid.Error != nil {
+		return "", fmt.Errorf("mojang api: %s", *uuid.Error)
 	}
 
 	return *uuid.ID, nil
@@ -25,19 +37,16 @@ func UUIDtoUsername(UUID string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer res.Body.Close()
 
 	var username MCResponse
-
 	if err := json.NewDecoder(res.Body).Decode(&username); err != nil {
 		return "", err
 	}
 
-	return *username.Name, nil
-}
+	if username.Error != nil {
+		return "", fmt.Errorf("mojang api: %s", *username.Error)
+	}
 
-type MCResponse struct {
-	Name  *string `json:"name"`
-	ID    *string `json:"id"`
-	Error *string `json:"errorMessage"`
-	Path  *string `json:"path"`
+	return *username.Name, nil
 }
